86693c62a003da570c34e0b806ea15f61a97c23e
queenJiJi <lovebellaberry52@gmail.com>
2025-07-09 22:06:05 +0900

    [feat] 마크다운 적용

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getPostData } from "@/api/posts";
+import MarkdownViewer from "@/components/MarkdownViewer";
 import React from "react";
 
 export default async function Postpage({
@@ -6,13 +7,13 @@ export default async function Postpage({
 }: {
   params: { slug: string };
 }) {
-  // 1. 전달된 slug에 해당하는 포스트 데이터를 읽어와서
-  // 2. 데이터를 마크다운 뷰어에 표기하기
   const post = await getPostData(slug);
   return (
     <section className="p-8 sm:p-20">
       <h1 className="font-bold text-3xl">{post.title}</h1>
-      <pre className="">{post.content}</pre>
+      <div>
+        <MarkdownViewer content={post.content} />
+      </div>
     </section>
   );
 }
---
+++ src/components/MarkdownViewer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+export default function MarkdownViewer({ content }: { content: string }) {
+  return (
+    <article className="prose max-w-none lg:prose-xl">
+      <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+      {/* <Markdown>{content}</Markdown> */}
+      {/* <pre>{content</pre> */}
+    </article>
+  );
+}
